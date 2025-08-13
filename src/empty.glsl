0ad7e89c75b66dae9734e3d86f04ad7edd476ecf
Pengo Wray <me@pengowray.com>
2025-08-13 11:35:45 +1000

    update default glsl text

--- src/empty.glsl
+++ src/empty.glsl
@@ -2,9 +2,14 @@
 // the parameter "cs" is a screen coordinate - it ranges from (0, 0) in the top left
 // to (1.0, 1.0) in the bottom right
 // available uniforms:
-// float time - time in seconds since shader started
 // vec2 resolution - display resolution in pixels (it's 1920x1080 :3)
+// float time - time in seconds since shader started
 // float chat_time - timestamp (in seconds since shader started) of last chat message
+// float tracking_mouth - how open my mouth is (1.0 = Fully Open, 0.0 = Fully Antiopen)
+// vec2 tracking_eyes - how open each eye is (1.0 = Fully Open Also)
+// vec2 emacs_cursor - position of my emacs cursor on screen (in pixels)
+// vec2 mouse_cursor - position of my mouse cursor on screen (in pixels)
+// int heartrate - current rate at which my heart is beating (in Hz)
 vec4 shade(vec2 cs) {
     return vec4(0.0, 1.0, 0.0, 1.0);
 }
